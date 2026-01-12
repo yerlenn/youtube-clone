e11de9d36257c654617a58967af7decba031550d
Yerlen Saifulla <yrlnsayfulla@gmail.com>
2026-01-12 23:51:19 +0900

    Added local video processing endpoint

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,30 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.post('/process-video', (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
 
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad request: Missing file path.")
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") // 360p
+    .on("end", () => {
+      res.status(200).send("Video processing complete")
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`)
+      res.status(500).send(`Internal server error: ${err.message}`)
+    })
+    .save(outputFilePath)
+})
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Video processing is running")
 });
